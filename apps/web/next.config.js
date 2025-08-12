08fd0948615e6fb2f030a4ed3a3f1e68f972954a
Michael Hancock <onykage@gmail.com>
2025-08-11 19:45:28 -0500

    chatgpt recommended changes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {}
+  typescript: { tsconfigPath: "./tsconfig.json" }
 };
 export default nextConfig;
